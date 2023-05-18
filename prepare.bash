1a129e2d9e40abb46740119e7a24cdbd1d596b26
Nathan Pierce <norsegaud@gmail.com>
2023-05-17 21:14:16 -0400

    support both exit codes in prepare script cleanup

--- prepare.bash
+++ prepare.bash
@@ -5,8 +5,9 @@ SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
 . "${SCRIPT_DIR}/shared.bash"
 ${CUSTOM_ENV_ANKA_ENABLE_JOB_DEBUG_LOGGING:-false} && env | grep "ANKA_"
 set -eo pipefail
-# ensure we only ever retry the specific step, not the whole job (and all steps)
+# LAST_RC will allow us to capture if a function throws $BUILD_FAILURE_EXIT_CODE (not retryable) or $SYSTEM_FAILURE_EXIT_CODE (retryable)
 cleanup () {
-  exit $SYSTEM_FAILURE_EXIT_CODE
+  LAST_RC=$?
+  exit $LAST_RC
 }
 trap cleanup ERR
