7cf70f1067fbf0ea9c3dbefc084e81ae5b04703a
Nathan Pierce <NorseGaud@users.noreply.github.com>
2023-08-01 10:58:48 -0400

    new trap & env name

--- run.bash
+++ run.bash
@@ -5,9 +5,9 @@ SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
 . "${SCRIPT_DIR}/shared.bash"
 set -eo pipefail
 # ensure we only ever retry the specific step, not the whole job (and all steps)
-cleanup () {
-  exit $SYSTEM_FAILURE_EXIT_CODE
+cleanup_run () {
+  [[ $? -eq $BUILD_FAILURE_EXIT_CODE ]] && exit $BUILD_FAILURE_EXIT_CODE || exit $RETRY_STEP_EXIT_CODE
 }
-trap cleanup ERR
+trap cleanup_run ERR
 # ${@: -2} is necessary since config.toml run_args come before the actual script and step name arguments
 "${@: -2}"
