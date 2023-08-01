b7da2f31817835a2f3e4ddaa2c80fd847b8dc4cd
Nathan Pierce <NorseGaud@users.noreply.github.com>
2023-08-01 10:57:07 -0400

    better cleanup

--- cleanup.bash
+++ cleanup.bash
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
+# cleanup stage does not retry using $SYSTEM_FAILURE_EXIT_CODE
 echo "] Cleaning..."
 SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
 . "${SCRIPT_DIR}/shared.bash"
 set -eo pipefail
-# cleanup stage does not retry using $SYSTEM_FAILURE_EXIT_CODE
-RETRIES=3
-while ! (
-  [[ $RETRIES -gt 0 ]] || (echo "ERROR: cleanup hit max retries" && exit $BUILD_FAILURE_EXIT_CODE)
-  echo "cleanup logic here"
-) ; do
-  ((RETRIES=RETRIES-1))
-  sleep 1
+cleanup () {
+  echo "logic you have to ensure runs, regardless of failures"
+}
+trap cleanup EXIT
+RETRIES=4
+while [[ $((RETRIES=RETRIES-1)) -gt 0 ]] || (echo "ERROR: cleanup hit max retries" && false); do
+  sleep 5
+  (
+    echo "cleanup code here"
+  ) || [[ $? -ne $RETRY_STEP_EXIT_CODE ]] && break
 done
+
