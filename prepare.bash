cdd9b0013d5fb7bc7e9f6aace6d24564cbd7a8ec
Nathan Pierce <NorseGaud@users.noreply.github.com>
2023-08-01 10:59:26 -0400

    new trap and env

--- prepare.bash
+++ prepare.bash
@@ -5,9 +5,8 @@ SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
 . "${SCRIPT_DIR}/shared.bash"
 ${CUSTOM_ENV_ANKA_ENABLE_JOB_DEBUG_LOGGING:-false} && env | grep "ANKA_"
 set -eo pipefail
-# LAST_RC will allow us to capture if a function throws $BUILD_FAILURE_EXIT_CODE (not retryable) or $SYSTEM_FAILURE_EXIT_CODE (retryable)
-cleanup () {
-  LAST_RC=$?
-  exit $LAST_RC
+# ensure we only ever retry the specific step, not the whole job (and all steps)
+cleanup_prepare () {
+  [[ $? -eq $BUILD_FAILURE_EXIT_CODE ]] && exit $BUILD_FAILURE_EXIT_CODE || exit $RETRY_STEP_EXIT_CODE
 }
-trap cleanup ERR
+trap cleanup_prepare ERR
