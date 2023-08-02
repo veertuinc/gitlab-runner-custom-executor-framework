05a2b994633c99f0b36832995ffbccbffbccf53f
Nathan Pierce <NorseGaud@users.noreply.github.com>
2023-08-02 13:19:41 -0400

    new cleanup exponential backoff

--- cleanup.bash
+++ cleanup.bash
@@ -8,11 +8,19 @@ cleanup () {
   echo "logic you have to ensure runs, regardless of failures"
 }
 trap cleanup EXIT
-RETRIES=4
-while [[ $((RETRIES=RETRIES-1)) -gt 0 ]] || (echo "ERROR: cleanup hit max retries" && false); do
-  sleep 5
-  (
-    echo "cleanup code here"
-  ) || [[ $? -ne $RETRY_STEP_EXIT_CODE ]] && break
+set -E # required to get exit code from while ()
+BASE=${BACKOFF_BASE:-1}
+MAX=${BACKOFF_MAX:-60}
+FAILURES=0
+while (
+  # cleanup logic here
+) 2>&1; RC=$?; [[ $RC -ne 0 ]]; do
+  FAILURES=$(( $FAILURES + 1 ))
+  SECONDS=$(( ($BASE * 2) ** ($FAILURES - 1) ))
+  if [[ $SECONDS -ge $MAX ]]; then
+    SECONDS=$MAX
+  fi
+  print_warning "$FAILURES failure(s), retrying in $SECONDS second(s)" >&2
+  sleep $SECONDS
+  echo
 done
-
